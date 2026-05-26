a0f3c4174b2b0c3f2e0eb085a0a544fd2b16ace8
liuzheng <liuzheng@inmolens.com>
2026-05-26 13:49:41 +0800

    Subject: 初始化playh5
    Module: [playh5]
    Type_ID: story:12345678
    Project: oppo
    Description:
    - 初始化playh5
    
    Change-Id: Ie599b771649f556a4f13eed75debcf48f1ef263e

--- src/main/java/com/bootdo/system/config/ShiroConfig.java
+++ src/main/java/com/bootdo/system/config/ShiroConfig.java
@@ -82,6 +82,7 @@ public class ShiroConfig {
         filterChainDefinitionMap.put("/druid/**", "anon");
         filterChainDefinitionMap.put("/upload/**", "anon");
         filterChainDefinitionMap.put("/files/**", "anon");
+        filterChainDefinitionMap.put("/tutorials-core/**", "anon");
         filterChainDefinitionMap.put("/v1/chat/completions", "anon");
         filterChainDefinitionMap.put("/v1/images/generations", "anon");
         filterChainDefinitionMap.put("/v1/videos/generations", "anon");
